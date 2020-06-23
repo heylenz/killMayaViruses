c07e02ae73a54f7781b38a8336598851d6320bb0
yinhailin <hailin.yin@outlook.com>
2020-06-23 20:59:39 +0800

    添加 mel修复

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ email: hailin.yin@outlook.com
 """
 import os
 import re
+import glob
 # import psutil
 import shutil
 
@@ -19,11 +20,37 @@ from rich.progress import track
 /plug-ins/animImportExport.pres.mel
 """
 
+def fixed_anim_import_export_mel(filename, force=False):
+    backup = f"{filename}.backup"
 
-def fixed_anim_import_export_mel(autodesk_root):
-    pass
+    if not force:
+        if os.path.exists(backup):
+            logger.warning(f"{filename} 文件已经执行过, 如果需要重新执行,请删除backup文件,或者还原重新执行")
+            return
 
+    shutil.copy2(filename, backup)
 
+    with open(backup) as fread:
+        with open(filename, 'w') as fwrite:
+
+            for line in fread:
+                if re.search('This script is machine generated', line):
+                    while fread.readline():
+                        logger.debug(line)
+                else:
+                    fwrite.write(line)
+
+@logger.catch
+def fixed_anim_import_export_mels(autodesk_root):
+    for version in [k for k in os.listdir(autodesk_root) if re.search("^Maya20", k)]:
+        # print(version)
+        for mel in glob.glob(f'{autodesk_root}/{version}/resources/l10n/*/plug-ins/animImportExport.pres.mel'):
+            # print(mel)
+    
+            # print(os.path.exists(mel))
+            fixed_anim_import_export_mel(mel)
+
+@logger.catch
 def check_maya_file(filename):
     with open(filename) as f:
         for line in f:
@@ -62,17 +89,26 @@ def fixed_maya_file(filename):
     logger.info(f"{filename} 已经删除中毒代码，请检查文件")
 
 
-def main(root, autodesk_root=r"C:\Program Files\Autodesk"):
-    """
-    default autodesk_root C:\\Program Files\\Autodesk
-    python main.py --root=d: --autodesk_root=c:\Program Files\Autodesk
-    """
+def main(root, autodesk_root=r"C:\Program Files\Autodesk", force=False):
+    """普天同庆查杀修复工具,默认会自动备份
+
+    Args:
+        root (str): 输入查杀目录
+        autodesk_root (regexp, optional): maya 安装根目录. Defaults to r"C:\Program Files\Autodesk".
+        force (bool, optional): 是否强制更新受影响的mel. Defaults to False.
+
+    Raises:
+        RuntimeError: 如果autodesk_root根目录不是以Autodesk 结尾 那么报错
+    """    
+    
     if autodesk_root[-8:] != 'Autodesk':
         raise RuntimeError('autodesk_root 参数不正确，参考 C:\Program Files\Autodesk')
-    fixed_anim_import_export_mel(autodesk_root)
+    fixed_anim_import_export_mels(autodesk_root)
 
     all_maya_files = []
 
+
+    logger.info(f"开始扫描盘符 {root}, 需要点时间, 请耐心等待")
     for _root, _dirs, files in os.walk(root):
         for f in files:
             if f.endswith(".ma"):
